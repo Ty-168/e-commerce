01e8b6dcde14f3be5be1d0a9adfe443f711da6c1
Ty-168 <kitylim123@gmail.com>
2025-10-18 15:05:40 +0700

    change size category image

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -9,8 +9,8 @@ const props = defineProps<{
 </script>
 
 <template>
-<div :style="{backgroundColor: color}" class="flex flex-col items-center justify-center w-32 h-40 rounded-md p-2">
-    <img :src="props.picture" alt="pic" class="w-16 h-16 object-contain"/> 
+<div :style="{backgroundColor: color}" class="flex flex-col items-center justify-center w-32 h-40 rounded-md p-4">
+    <img :src="props.picture" alt="pic" class="w-28 h-28 object-contain"/> 
     <h5 class="font-bold text-black text-sm text-center">{{ props.message }}</h5> 
     <span class="text-xs text-[#B6B6B6]">{{ props.amount }} items</span> 
   </div>    
